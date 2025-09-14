cdf1d07a81af2a51061415cc36c3dab1d66a91ab
mvareikis <mvareikis@paloaltonetworks.com>
2025-09-14 12:03:00 +0300

    count tokens

--- count_tokens.py
+++ count_tokens.py
@@ -7,13 +7,14 @@ text = "hey, what can I do in Tel Aviv at summer? Use internet to google for pos
 
 # Initialize client for Vertex AI (uses your gcloud auth / service account)
 dotenv.load_dotenv()
-client = genai.Client(vertexai=True, project=os.environ["GCP_PROJECT"], location="europe-west4")
-
-# Call Gemini
-chat = client.chats.create(
-    model="gemini-2.5-flash-lite",
+client = genai.Client(
+    vertexai=True, project=os.environ["GCP_PROJECT"], location="europe-west4"
 )
 
-response = chat.send_message(text)
-print(response.text)
+# Call Gemini
+model = client.models.count_tokens(
+    model="gemini-2.5-flash-lite",
+    contents="What's the highest mountain in Africa?"
+)
 
+print(model)
